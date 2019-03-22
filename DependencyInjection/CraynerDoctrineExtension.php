91ad6f9e5c517db894768ea0b97ed3bab813960b
Craig Rayner <craig@craigrayner.com>
2019-03-22 14:55:35 +1100

    Add Extension File

---
+++ DependencyInjection/CraynerDoctrineExtension.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * doctrine-bundle
+ * (c) 2019 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 22/03/2019
+ * Time: 14:53
+ */
+namespace Crayner\Doctrine\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class CraynerDoctrineExtension
+ * @package Crayner\Doctrine\DependencyInjection
+ */
+class CraynerDoctrineExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $locator = new FileLocator(__DIR__ . '/../Resources/config');
+        $loader  = new YamlFileLoader(
+            $container,
+            $locator
+        );
+        $loader->load('services.yaml');
+    }
+
+}
